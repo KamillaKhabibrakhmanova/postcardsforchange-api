1cad743b9906ce74419c714032a7d92997685847
KamillaKhabibrakhmanova <venessiel@gmail.com>
2017-06-28 08:07:34 -0400

    headers

--- app.js
+++ app.js
@@ -34,6 +34,12 @@ app.use(cors(corsOptions))
 
 app.use(helmet());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // app.use(express.static('./app'));
 //app.use(favicon('./favicon.ico'));
 
