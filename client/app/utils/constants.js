d6a28a31273533c2b0b20cf5e603c71548299467
KamillaKhabibrakhmanova <venessiel@gmail.com>
2017-11-25 17:02:57 +0800

    add environment constant

--- client/app/utils/constants.js
+++ client/app/utils/constants.js
@@ -5,17 +5,22 @@
  */
 
 let baseUrl;
+let environment;
 
 switch(process.env.NODE_ENV) {
     case 'production':
         baseUrl='https://postcards4change.herokuapp.com/';
+        environment = 'production';
         break;
     case 'staging':
         baseUrl='https://postcards4change.herokuapp.com/';
+        environment = 'staging';
         break;
     default:
         baseUrl='http://localhost:8080/';
+        environment = 'development';
 
 }
 
 export const BASE_URL = baseUrl;
+export const ENVIRONMENT = environment;
