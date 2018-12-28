a652536a8753f0a742c9a995a36fbefe66eeab93
KamillaKhabibrakhmanova <venessiel@gmail.com>
2018-12-27 21:38:57 -0500

    update lob

--- services/lob.js
+++ services/lob.js
@@ -18,6 +18,61 @@ const getLobFormattedAddress = addressObject => {
 	};
 };
 
+const getBackPostcardTemplate = (politician, name, message) => {
+	return `<html>
+	<head>
+	<meta charset="UTF-8">
+	<title>Postcard for Change</title>
+	<style>
+	  *, *:before, *:after {
+		-webkit-box-sizing: border-box;
+		-moz-box-sizing: border-box;
+		box-sizing: border-box;
+	  }
+	  body {
+		width: 6.25in;
+		height: 4.25in;
+		margin: 0;
+		padding: 0;
+		background-color: white;
+	  }
+	  /* do not put text outside of the safe area */
+	  #safe-area {
+		position: absolute;
+		width: 5.875in;
+		height: 3.875in;
+		left: 0.1875in;
+		top: 0.1875in;
+	  }
+	  #message {
+		position: absolute;
+		width: 2.2in;
+		height: 2in;
+		top: 1.4in;
+		left: .25in;
+		font-family: 'Lato';
+		font-weight: 400;
+		font-size: .122in;
+	  }
+	</style>
+	</head>
+	<body>
+	  <!-- do not put text outside of the safe area -->
+	  <div id="safe-area">
+		<div id="message">
+		  <span class="accent">Dear ${politician},</span>
+		  <br><br>
+		  ${message}
+		  <br><br>
+		  Thank you,
+		  <br><br>
+		  ${name}
+		</div>
+	  </div>
+	</body>
+	</html>`
+}
+
 module.exports = {
 
 	//** sendIssuePostcard: send issue card via lob
@@ -42,7 +97,7 @@ module.exports = {
 			to: representativeAddress,
 			from: fromAddress,
 			description: issue.title,
-			back: `<p> ${issue.message} </p>`,
+			back: getBackPostcardTemplate(representative.name, from.name, issue.message),
 			front: issue.postcardImage || issue['postcard_image'] 
 		};
 
